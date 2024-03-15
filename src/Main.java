baa7befd168155d779260025b0f23daa7fa68c8f
Dreallidar <Realdreanix@gmail.com>
2024-03-15 15:51:19 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("~~~~~~~~~~Task 1~~~~~~~~~~~~~");
+
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("labas");
+        }
+        System.out.println("~~~~~~~~~~~~~~~~~Task 2~~~~~~~~~~~~~~~");
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("~~~~~~~~~~~~~~~~Task 3~~~~~~~~~~~~~~~~~~");
+
+
+    }
+}
