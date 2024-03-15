e3bd41efed48dbfc83213d2fdb82cf7c9cad8588
Dreallidar <Realdreanix@gmail.com>
2024-03-15 20:23:59 +0200

    Task 4 is done, and I  do not have suicidal thoughts I promise.

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,26 @@ public class Main {
 
         System.out.println("~~~~~~~~~~~~~~~~Task 3~~~~~~~~~~~~~~~~~~");
 
+        String[] augalai = new String[10];
+        augalai[0] = "Berzas";
+        augalai[1] = "Azuolas";
+        augalai[2] = "Obelis";
+        augalai[3] = "Pusis";
+        augalai[4] = "Egle";
+        augalai[5] = "Zibute";
+        augalai[6] = "Roze";
+        augalai[7] = "Piene";
+        augalai[8] = "Tabakas";
+        augalai[9] = "Marichuana";
+        System.out.println(augalai[2]);
 
+        System.out.println("~~~~~~~~~~~~~~~~Task 4~~~~~~~~~~~~~~~~~~");
+
+
+        for (int i = 0; i < augalai.length; i++) {
+        System.out.println(augalai[i]);
     }
+
+
+}
 }
